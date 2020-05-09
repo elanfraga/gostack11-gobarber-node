6600e095af70b95a200a47c09c6c8793f1704d0c
Elan Fraga <elanfraga@gmai.com>
2020-05-08 23:46:40 -0300

    Template de emails

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,14 +4,22 @@ import IStorageProvider from '@shared/container/providers/StorageProvider/models
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
-import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtheralMailProvider';
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
