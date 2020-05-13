ad824cdac635faaa7dc2157d2addefc0c3f0d531
Elan Fraga <elanfraga@gmai.com>
2020-05-13 01:02:42 -0300

    Utilizando Class Transformer

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,21 +1,20 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
-export default class SessionsController {
+export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
-    const authenticateUserService = container.resolve(AuthenticateUserService);
+    const authenticateUser = container.resolve(AuthenticateUserService);
 
-    const { user, token } = await authenticateUserService.execute({
+    const { user, token } = await authenticateUser.execute({
       email,
       password,
     });
 
-    delete user.password;
-
-    return response.json({ user, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
