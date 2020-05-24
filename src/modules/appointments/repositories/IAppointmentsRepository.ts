458b223e7a29e7df13cf0f0daedf61b9f56042f2
Elan Fraga <elanfraga@gmail.com>
2020-05-24 17:38:48 -0300

    Dias indisponíveis no mês

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,11 +1,11 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
-import IFindAllInMonthFromProvideDTO from '@modules/appointments/dtos/IFindAllInMonthFromProvideDTO';
-import IFindAllInDayFromProvideDTO from '@modules/appointments/dtos/IFindAllInDayFromProvideDTO';
+import IFindAllInMonthFromProvideDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProvideDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provide_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProvideDTO,
   ): Promise<Appointment[]>;
