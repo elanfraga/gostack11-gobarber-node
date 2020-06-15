6ddd3ac19c55e8c45bab35ba9c710f28efd539b9
Elan Fraga <elanfraga@gmail.com>
2020-06-14 21:39:12 -0300

    correçao do nome de interface

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,9 +1,9 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
-import IFindProvidersDTO from '@modules/appointments/dtos/IFindProvidersDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 export default interface IUserRepository {
-  findAllProviders(data: IFindProvidersDTO): Promise<User[]>;
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
